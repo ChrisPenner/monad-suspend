6c73544e6dbe4fae24634c00511a2af9931d90d6
Chris Penner <christopher.penner@gmail.com>
2019-09-07 18:31:59 -0600

    User ReaderT for condition

--- src/Control/Monad/Suspend.hs
+++ src/Control/Monad/Suspend.hs
@@ -7,6 +7,7 @@ module Control.Monad.Suspend where
 import Control.Monad.Coroutine
 import Control.Monad.Coroutine.SuspensionFunctors
 import Control.Monad.State
+import Control.Monad.Reader
 import Data.Monoid
 
 class MonadSuspend m o | m -> o where
@@ -15,19 +16,13 @@ class MonadSuspend m o | m -> o where
     adjust :: (o -> o) -> m ()
 
 instance (Monad m) => MonadSuspend (SuspendT o m) o where
-  adjust f = SuspendT . lift $ modifyCurrentCost f
-
-data Costs o = Costs {currentCost :: o, condition :: o -> Bool}
+  adjust f = SuspendT . lift $ modify f
 
 newtype SuspendT o m a =
-    SuspendT { runSuspend :: Coroutine (Request (Costs o) (Costs o)) (StateT (Costs o) m) a
+    SuspendT { runSuspend :: Coroutine (Request o o) (ReaderT (o -> Bool) (StateT o m)) a
              }
     deriving stock Functor
     deriving newtype MonadIO
-
-modifyCurrentCost :: MonadState (Costs o) m => (o -> o) -> m ()
-modifyCurrentCost f = do
-    modify (\c -> c{currentCost = f (currentCost c)})
 
 instance (Monad m) => Applicative (SuspendT o m) where
   pure = SuspendT . pure
@@ -37,12 +32,13 @@ instance (Monad m) => Monad (SuspendT o m) where
   return = SuspendT . return
   SuspendT x >>= f = SuspendT $ do
       x' <- x
-      costs@(Costs currentCost costCondition) <- lift get
-      when (costCondition currentCost) (request costs >>= lift . put)
+      currentCost <- lift get
+      costCondition <- lift ask
+      when (costCondition currentCost) (request currentCost >>= lift . put)
       runSuspend $ f x'
 
 suspendAtCost :: (Monad m) => o -> (o -> Bool) -> SuspendT o m a -> m (Either (SuspendT o m a) a)
-suspendAtCost startCost condition (SuspendT crt) = flip evalStateT (Costs startCost condition) $ do
+suspendAtCost startCost condition (SuspendT crt) = flip evalStateT startCost . flip runReaderT condition $ do
     resume crt >>= \case
         Left (Request cst continue) -> return . Left . SuspendT $ continue cst
         Right a -> return $ Right a
