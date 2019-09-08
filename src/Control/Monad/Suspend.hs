9c793382e3db40fdc9bc2dc6550fed70d94077f5
Chris Penner <christopher.penner@gmail.com>
2019-09-07 18:47:52 -0600

    Cleanup

--- src/Control/Monad/Suspend.hs
+++ src/Control/Monad/Suspend.hs
@@ -9,6 +9,8 @@ import Control.Monad.Coroutine.SuspensionFunctors
 import Control.Monad.State
 import Control.Monad.Reader
 import Data.Monoid
+import Data.Functor
+import Data.Bifunctor
 
 class MonadSuspend m o | m -> o where
     spend :: o -> m ()
@@ -19,7 +21,7 @@ instance (Monad m) => MonadSuspend (SuspendT o m) o where
   adjust f = SuspendT . lift $ modify f
 
 newtype SuspendT o m a =
-    SuspendT { runSuspend :: Coroutine (Request o o) (ReaderT (o -> Bool) (StateT o m)) a
+    SuspendT { runSuspend :: Coroutine (Yield ()) (ReaderT (o -> Bool) (StateT o m)) a
              }
     deriving stock Functor
     deriving newtype MonadIO
@@ -32,16 +34,22 @@ instance (Monad m) => Monad (SuspendT o m) where
   return = SuspendT . return
   SuspendT x >>= f = SuspendT $ do
       x' <- x
+      lift ask <*> lift get
       currentCost <- lift get
       costCondition <- lift ask
-      when (costCondition currentCost) (request currentCost >>= lift . put)
+      when (costCondition currentCost) (yield ())
       runSuspend $ f x'
 
-suspendAtCost :: (Monad m) => o -> (o -> Bool) -> SuspendT o m r -> m (Either (SuspendT o m r) r, o)
-suspendAtCost startCost condition (SuspendT crt) = flip runStateT startCost . flip runReaderT condition $ do
-    resume crt >>= \case
-        Left (Request cst continue) -> return . Left . SuspendT $ continue cst
-        Right a -> return $ Right a
+suspendAtCost :: (Monad m)
+              => o
+              -> (o -> Bool)
+              -> SuspendT o m r
+              -> m (Either (SuspendT o m r) r, o)
+suspendAtCost startCost condition (SuspendT coroutine) =
+    flip runStateT startCost . flip runReaderT condition $ do
+        resume coroutine <&> \case
+            Left (Yield () continue) -> Left . SuspendT $ continue
+            Right a -> Right a
 
 test :: SuspendT (Sum Int) IO ()
 test = do
